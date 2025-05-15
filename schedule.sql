bc8368d2ac3dbfa3714f62d2115b837c199a3f11
NewJKH <newsun0805@gmail.com>
2025-05-15 11:42:30 +0900

    modify: LV 0 API 명세서 및 ERD 작성 - SQL 작성하기

--- schedule.sql
+++ schedule.sql
@@ -1,6 +1,6 @@
 /* LV 0 테이블 생성하기 */
 CREATE TABLE `schedule`.`schedules` (
-    `member_name` INT NOT NULL ,
+    `member_name` VARCHAR(20) NOT NULL ,
     `title` VARCHAR(20) NOT NULL ,
     `content` TEXT NOT NULL ,
     `created_at` DATETIME NOT NULL,
