25c68120e2de3143f5c9bf0da5b658634f90d9e5
NewJKH <newsun0805@gmail.com>
2025-05-17 17:03:57 +0900

    perf: "Lv 2. 유저 CRUD : 연관간계 설정 수정"

--- src/main/java/org/jkh/scheduleapi/domain/schedule/entity/Schedule.java
+++ src/main/java/org/jkh/scheduleapi/domain/schedule/entity/Schedule.java
@@ -1,10 +1,7 @@
 package org.jkh.scheduleapi.domain.schedule.entity;
 
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.jkh.scheduleapi.common.entity.BaseDate;
@@ -16,6 +13,9 @@ import org.jkh.scheduleapi.common.entity.BaseDate;
 public class Schedule extends BaseDate {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     @Column(length = 20, nullable = false)
     private String member_name;
 
