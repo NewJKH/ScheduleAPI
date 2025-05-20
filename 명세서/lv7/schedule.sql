1fac7d109ce89e05cb876bb0e73f8b5dea600c91
NewJKH <newsun0805@gmail.com>
2025-05-20 16:19:33 +0900

    perf: "Lv.7 댓글 CRUD : 연관관계 매핑을 위한 테이블 수정"

--- 명세서/lv7/schedule.sql
+++ 명세서/lv7/schedule.sql
@@ -1,24 +1,24 @@
 CREATE TABLE members
 (
-    id          BIGINT      NOT NULL AUTO_INCREMENT,
+    member_id   BIGINT      NOT NULL AUTO_INCREMENT,
     member_name VARCHAR(20) NOT NULL,
     password    VARCHAR(20) NOT NULL,
     email       VARCHAR(255),
     created_at  DATETIME    NOT NULL,
     modified_at DATETIME    NOT NULL,
-    PRIMARY KEY (id)
+    PRIMARY KEY (member_id)
 );
 
 CREATE TABLE schedules
 (
-    id          BIGINT      NOT NULL AUTO_INCREMENT,
-    member_name VARCHAR(20) NOT NULL,
+    schedule_id BIGINT      NOT NULL AUTO_INCREMENT,
+    member_id   VARCHAR(20) NOT NULL,
     title       VARCHAR(20) NOT NULL,
     content     TEXT        NOT NULL,
     created_at  DATETIME    NOT NULL,
     modified_at DATETIME    NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (member_name) REFERENCES members (member_name)
+    PRIMARY KEY (schedule_id),
+    FOREIGN KEY (member_id) REFERENCES members (member_id)
 );
 
 CREATE TABLE comments
@@ -30,7 +30,7 @@ CREATE TABLE comments
     created_at  DATETIME NOT NULL,
     modified_at DATETIME NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (member_id) REFERENCES members (id),
-    FOREIGN KEY (schedule_id) REFERENCES schedules (id)
+    FOREIGN KEY (member_id) REFERENCES members (member_id),
+    FOREIGN KEY (schedule_id) REFERENCES schedules (schedule_id)
 );
 
