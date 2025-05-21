efcf8e89bd2451097c331f1ce84561d55586b1aa
NewJKH <newsun0805@gmail.com>
2025-05-21 14:12:02 +0900

    feat: "기타 파일 추가"

---
+++ src/main/java/org/jkh/scheduleapi/ScheduleApiApplication.java
@@ -0,0 +1,15 @@
+package org.jkh.scheduleapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class ScheduleApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleApiApplication.class, args);
+    }
+
+}
