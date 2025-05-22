d24e742b51421e1ca073ad74942e70a0fa8fb2e9
NewJKH <newsun0805@gmail.com>
2025-05-22 14:26:15 +0900

    perf: "댓글 수정, 삭제 댓글 ID 별도 분리"

--- src/main/java/org/jkh/scheduleapi/domain/comment/dto/CommentDeleteRequest.java
+++ src/main/java/org/jkh/scheduleapi/domain/comment/dto/CommentDeleteRequest.java
@@ -3,6 +3,5 @@ package org.jkh.scheduleapi.domain.comment.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public record CommentDeleteRequest(
-        Long id,
         @JsonProperty("member_id")
         Long memberId) { }
--- src/main/java/org/jkh/scheduleapi/domain/comment/dto/CommentUpdateRequest.java
+++ src/main/java/org/jkh/scheduleapi/domain/comment/dto/CommentUpdateRequest.java
@@ -3,7 +3,6 @@ package org.jkh.scheduleapi.domain.comment.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public record CommentUpdateRequest(
-        Long id,
         @JsonProperty("schedule_id")
         Long scheduleId,
         @JsonProperty("member_id")
