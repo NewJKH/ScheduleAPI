7a2df88c09c85dfa716390604968deb4470d3730
NewJKH <newsun0805@gmail.com>
2025-05-22 14:05:27 +0900

    feat: "일정 업데이트 DTO 생성 추가"

---
+++ src/main/java/org/jkh/scheduleapi/domain/schedule/dto/request/ScheduleUpdateRequest.java
@@ -0,0 +1,13 @@
+package org.jkh.scheduleapi.domain.schedule.dto.request;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
+
+@Getter
+@Setter
+public class ScheduleUpdateRequest {
+    @Length(min = 1, max = 10)
+    private String title;
+    private String content;
+}
