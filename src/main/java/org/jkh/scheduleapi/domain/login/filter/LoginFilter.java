4b87e8bd4d990395c45bfc2b0bb5b9348995e490
NewJKH <newsun0805@gmail.com>
2025-05-23 12:41:45 +0900

    perf: "로그인 필터: 예외 처리방식 변경"

--- src/main/java/org/jkh/scheduleapi/domain/login/filter/LoginFilter.java
+++ src/main/java/org/jkh/scheduleapi/domain/login/filter/LoginFilter.java
@@ -2,35 +2,45 @@ package org.jkh.scheduleapi.domain.login.filter;
 
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
-import lombok.RequiredArgsConstructor;
 import org.jkh.scheduleapi.common.config.AuthProperties;
 import org.jkh.scheduleapi.common.exception.login.LoginRequiredException;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 import org.springframework.util.AntPathMatcher;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 
 import java.io.IOException;
 
 @Component
-@RequiredArgsConstructor
 public class LoginFilter implements Filter {
 
     private final AuthProperties authProperties;
     private final AntPathMatcher pathMatcher = new AntPathMatcher();
+    private final HandlerExceptionResolver resolver;
+
+    public LoginFilter(AuthProperties authProperties,@Qualifier("handlerExceptionResolver") HandlerExceptionResolver resolver) {
+        this.authProperties = authProperties;
+        this.resolver = resolver;
+    }
 
     @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+            throws IOException, ServletException {
         HttpServletRequest httpRequest = (HttpServletRequest) servletRequest;
+        HttpServletResponse httpResponse = (HttpServletResponse) servletResponse;
         String requestURI = httpRequest.getRequestURI();
-
         if ( !isWhitelist(requestURI) ){
             HttpSession session = httpRequest.getSession(false);
             if ( session == null || session.getAttribute("loginMember") == null ){
-                throw new LoginRequiredException();
+                resolver.resolveException(httpRequest, httpResponse, null, new LoginRequiredException());
             }
         }
 
         filterChain.doFilter(servletRequest,servletResponse);
+
+
     }
 
     private boolean isWhitelist(String requestURI) {
