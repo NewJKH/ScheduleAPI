aca6409bbe828e92167f81706fa2ba0ab32f39bf
NewJKH <newsun0805@gmail.com>
2025-05-24 14:12:50 +0900

    fix: "Email 객체 생성되던 문제 수정"

--- src/main/java/org/jkh/scheduleapi/common/entity/Email.java
+++ src/main/java/org/jkh/scheduleapi/common/entity/Email.java
@@ -1,5 +1,6 @@
 package org.jkh.scheduleapi.common.entity;
 
 public class Email {
+    private Email(){}
     public static final String emailPattern = "^[\\w!#$%&'*+/=?`{|}~^.-]+@[\\w.-]+\\.[a-zA-Z]{2,6}$";
 }
